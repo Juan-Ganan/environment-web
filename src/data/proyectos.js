2ebdb333bf3c857863254c7e37e78cf9048217e1
JuanDa <juan.ganan@utp.edu.co>
2026-02-02 22:05:50 -0500

    Fix bug environment VR trafo

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -2,11 +2,11 @@ export const proyectos = {
   wuka: {
     titulo: "Robot WUKA VR",
     descripcion: "Visualización de un robot industrial WUKA usando A-Frame y WebXR.",
-    entorno: true,
+    entorno: "/Entorno",
     web: null,
   },
 
-  bicineta: {
+  Bicineta: {
     titulo: "Proyecto Bicineta GCA",
     descripcion: "Sistema de monitoreo y visualización de una bicicleta eléctrica.",
     entorno: false,
